e308cfdf68cff3222da07dcddcb03a0ae3224b7c
Ilian Ranguelov <me@radarlog.net>
2019-03-06 21:07:16 +0100

    list images in controller

--- src/Infrastructure/Http/Controller/Index.php
+++ src/Infrastructure/Http/Controller/Index.php
@@ -4,13 +4,26 @@ declare(strict_types=1);
 namespace Radarlog\S3Uploader\Infrastructure\Http\Controller;
 
 use Radarlog\S3Uploader\Infrastructure\Http\Controller;
+use Radarlog\S3Uploader\Infrastructure\S3\Client;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation;
 
 final class Index extends AbstractController implements Controller
 {
+    /** @var Client */
+    private $client;
+
+    public function __construct(Client $client)
+    {
+        $this->client = $client;
+    }
+
     public function __invoke(HttpFoundation\Request $request): HttpFoundation\Response
     {
-        return $this->render('base.html.twig');
+        $images = iterator_to_array($this->client->list());
+
+        return $this->render('base.html.twig', [
+            'images' => $images,
+        ]);
     }
 }
