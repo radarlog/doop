ae388da4ed90932e4567b16d587ab58b85297fc8
Ilian Ranguelov <me@radarlog.net>
2019-03-06 22:03:26 +0100

    minor improvements

--- src/Domain/Storage.php
+++ src/Domain/Storage.php
@@ -6,4 +6,6 @@ namespace Radarlog\S3Uploader\Domain;
 interface Storage
 {
     public function upload(Image $image): void;
+
+    public function list(): \Iterator;
 }
--- src/Infrastructure/Http/Controller/Index.php
+++ src/Infrastructure/Http/Controller/Index.php
@@ -3,27 +3,25 @@ declare(strict_types=1);
 
 namespace Radarlog\S3Uploader\Infrastructure\Http\Controller;
 
+use Radarlog\S3Uploader\Domain\Storage;
 use Radarlog\S3Uploader\Infrastructure\Http\Controller;
-use Radarlog\S3Uploader\Infrastructure\S3\Client;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation;
 
 final class Index extends AbstractController implements Controller
 {
-    /** @var Client */
+    /** @var Storage */
     private $client;
 
-    public function __construct(Client $client)
+    public function __construct(Storage $client)
     {
         $this->client = $client;
     }
 
     public function __invoke(HttpFoundation\Request $request): HttpFoundation\Response
     {
-        $images = iterator_to_array($this->client->list());
-
         return $this->render('base.html.twig', [
-            'images' => $images,
+            'images' => $this->client->list(),
         ]);
     }
 }
