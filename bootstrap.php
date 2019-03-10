ff1f9b7e9bd96d47c8f5f95e0c89e87602a3f966
Ilian Ranguelov <me@radarlog.net>
2019-03-10 13:38:33 +0100

    check entry points too

--- bootstrap.php
+++ bootstrap.php
@@ -1,23 +1,31 @@
 <?php
 declare(strict_types=1);
 
+use Symfony\Component\Debug\Debug;
 use Symfony\Component\Dotenv\Dotenv;
 
 require __DIR__ . '/vendor/autoload.php';
 
 // Load cached env vars if the .env.local.php file exists
 // Run "composer dump-env prod" to create it (requires symfony/flex >=1.2)
-if (is_array($env = @include __DIR__ . '/.env.local.php')) {
+$env = @include_once __DIR__ . '/.env.local.php';
+
+if (is_array($env)) {
     $_SERVER += $env;
     $_ENV += $env;
 } elseif (!class_exists(Dotenv::class)) {
-    throw new RuntimeException('Please run "composer require symfony/dotenv" to load the ".env" files configuring the application.');
+    throw new RuntimeException('Please run "composer require symfony/dotenv"');
 } else {
     // load all the .env files
     (new Dotenv())->loadEnv(__DIR__ . '/.env');
 }
 
-$_SERVER['APP_ENV'] = $_ENV['APP_ENV'] = ($_SERVER['APP_ENV'] ?? $_ENV['APP_ENV'] ?? null) ?: 'dev';
+$_SERVER['APP_ENV'] = $_ENV['APP_ENV'] = $_SERVER['APP_ENV'] ?? $_ENV['APP_ENV'] ?? null ?: 'dev';
 $_SERVER['APP_DEBUG'] = $_SERVER['APP_DEBUG'] ?? $_ENV['APP_DEBUG'] ?? 'prod' !== $_SERVER['APP_ENV'];
 $_SERVER['APP_DEBUG'] = $_ENV['APP_DEBUG'] =
     (int)$_SERVER['APP_DEBUG'] || filter_var($_SERVER['APP_DEBUG'], FILTER_VALIDATE_BOOLEAN) ? '1' : '0';
+
+if ($_SERVER['APP_DEBUG']) {
+    umask(0000);
+    Debug::enable();
+}
