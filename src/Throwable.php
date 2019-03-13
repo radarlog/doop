5cf9176484b38e1504e38be087e560b495a92d1e
Ilian Ranguelov <me@radarlog.net>
2019-03-13 23:38:38 +0100

    introduce Image\State

--- src/Throwable.php
+++ src/Throwable.php
@@ -8,6 +8,7 @@ interface Throwable extends \Throwable
     # @formatter:off
     public const CODE_IMAGE          = 1000;
     public const CODE_UUID           = 1001;
+    public const CODE_STATE          = 1002;
 
     public const CODE_HANDLER        = 2000;
     public const CODE_MYSQL_SEVERS   = 3000;
