4a256c389057766f0ff9603d052c67b149e790c3
Ilian Ranguelov <me@radarlog.net>
2019-03-22 00:02:47 +0100

    use query to list images

--- src/Infrastructure/Http/Controller/Index.php
+++ src/Infrastructure/Http/Controller/Index.php
@@ -3,26 +3,26 @@ declare(strict_types=1);
 
 namespace Radarlog\S3Uploader\Infrastructure\Http\Controller;
 
-use Radarlog\S3Uploader\Domain\Storage;
+use Radarlog\S3Uploader\Application\Query;
 use Radarlog\S3Uploader\Infrastructure\Http\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation;
 
 final class Index extends AbstractController implements Controller
 {
-    /** @var Storage */
-    private $client;
+    /** @var Query\Image\FindAll */
+    private $findAll;
 
-    public function __construct(Storage $client)
+    public function __construct(Query\Image\FindAll $findAll)
     {
-        $this->client = $client;
+        $this->findAll = $findAll;
     }
 
     // phpcs:disable SlevomatCodingStandard.Functions.UnusedParameter.UnusedParameter
     public function __invoke(HttpFoundation\Request $request): HttpFoundation\Response
     {
         return $this->render('base.html.twig', [
-            'images' => $this->client->list(),
+            'images' => $this->findAll->sortedByUploadDate(),
         ]);
     }
 }
