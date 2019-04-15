3723fede73a696414d5126d4afe4025231a84e5b
Ilian Ranguelov <me@radarlog.net>
2019-04-15 20:34:45 +0200

    fix doc block

--- src/Application/Query/Image/FindAll.php
+++ src/Application/Query/Image/FindAll.php
@@ -6,7 +6,7 @@ namespace Radarlog\S3Uploader\Application\Query\Image;
 interface FindAll
 {
     /**
-     * @return array&UuidName
+     * @return array&UuidName[]
      */
     public function sortedByUploadDate(): array;
 }
