49ff234f216b210473e81b524f2b46e775bcf16a
Ilian Ranguelov <me@radarlog.net>
2019-04-16 21:10:53 +0200

    improvements around exceptions

--- src/Application/Command/Bus.php
+++ src/Application/Command/Bus.php
@@ -4,12 +4,14 @@ declare(strict_types=1);
 namespace Radarlog\S3Uploader\Application\Command;
 
 use Radarlog\S3Uploader\Application\Command;
+use Radarlog\S3Uploader\Domain;
 
 interface Bus
 {
     /**
      * Execute the command
      *
+     * @throws Domain\Throwable
      * @throws RuntimeException
      */
     public function execute(Command $command): void;
--- src/Throwable.php
+++ src/Throwable.php
@@ -6,13 +6,19 @@ namespace Radarlog\S3Uploader;
 interface Throwable extends \Throwable
 {
     # @formatter:off
+
+    # DOMAIN LAYER
     public const CODE_IMAGE          = 1000;
     public const CODE_UUID           = 1001;
     public const CODE_STATE          = 1002;
     public const CODE_NAME           = 1003;
     public const CODE_HASH           = 1004;
 
+    # APPLICATION LAYER
     public const CODE_HANDLER        = 2000;
+
+    # INFRASTRUCTURE LAYER
     public const CODE_MYSQL_SEVERS   = 3000;
+
     # @formatter:on
 }
