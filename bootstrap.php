9a7babf580936e87a7af10a9ce1129253a15ae9e
Ilian Ranguelov <me@radarlog.net>
2019-10-08 14:35:00 +0300

    apply PSR12 and new sniffs

--- bootstrap.php
+++ bootstrap.php
@@ -1,4 +1,5 @@
 <?php
+
 declare(strict_types=1);
 
 use Symfony\Component\Debug\Debug;
@@ -20,10 +21,13 @@ if (is_array($env)) {
     (new Dotenv())->loadEnv(__DIR__ . '/.env');
 }
 
-$_SERVER['APP_ENV'] = $_ENV['APP_ENV'] = $_SERVER['APP_ENV'] ?? $_ENV['APP_ENV'] ?? null ?: 'dev';
-$_SERVER['APP_DEBUG'] = $_SERVER['APP_DEBUG'] ?? $_ENV['APP_DEBUG'] ?? 'prod' !== $_SERVER['APP_ENV'];
-$_SERVER['APP_DEBUG'] = $_ENV['APP_DEBUG'] =
+$_ENV['APP_ENV'] = $_SERVER['APP_ENV'] ?? $_ENV['APP_ENV'] ?? null ?: 'dev';
+$_SERVER['APP_ENV'] = $_ENV['APP_ENV'];
+
+$_SERVER['APP_DEBUG'] = $_SERVER['APP_DEBUG'] ?? $_ENV['APP_DEBUG'] ?? $_SERVER['APP_ENV'] !== 'prod';
+$_ENV['APP_DEBUG'] =
     (int) $_SERVER['APP_DEBUG'] || filter_var($_SERVER['APP_DEBUG'], FILTER_VALIDATE_BOOLEAN) ? '1' : '0';
+$_SERVER['APP_DEBUG'] = $_ENV['APP_DEBUG'];
 
 if ($_SERVER['APP_DEBUG']) {
     umask(0000);
--- src/Infrastructure/Http/Form/UploadType.php
+++ src/Infrastructure/Http/Form/UploadType.php
@@ -1,4 +1,5 @@
 <?php
+
 declare(strict_types=1);
 
 namespace Radarlog\Doop\Infrastructure\Http\Form;
