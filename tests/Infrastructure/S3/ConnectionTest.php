e78ec60fe9c279de423b25d008717bebb259bbe1
Ilian Ranguelov <me@radarlog.net>
2020-04-01 22:49:52 +0200

    improve S3\ConnectionTest

--- tests/Infrastructure/S3/ConnectionTest.php
+++ tests/Infrastructure/S3/ConnectionTest.php
@@ -15,6 +15,15 @@ class ConnectionTest extends UnitTestCase
     private const SECRET = 'secret';
     private const REGION = 'region';
 
+    private Connection $connection;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->connection = Connection::from(self::ENDPOINT, self::KEY, self::SECRET, self::REGION);
+    }
+
     public function testFrom(): void
     {
         $endpoint = new Connection\Endpoint(self::ENDPOINT);
@@ -22,35 +31,28 @@ class ConnectionTest extends UnitTestCase
         $secret = new Connection\Key(self::SECRET);
         $region = new Connection\Region(self::REGION);
 
-        $connection1 = new Connection($endpoint, $key, $secret, $region);
-        $connection2 = Connection::from(self::ENDPOINT, self::KEY, self::SECRET, self::REGION);
+        $connection = new Connection($endpoint, $key, $secret, $region);
 
-        self::assertEquals($connection1, $connection2);
+        self::assertEquals($connection, $this->connection);
     }
 
     public function testRegion(): void
     {
-        $connection = Connection::from(self::ENDPOINT, self::KEY, self::SECRET, self::REGION);
-
-        $configuration = $connection->configuration();
+        $configuration = $this->connection->configuration();
 
         self::assertSame(self::REGION, $configuration->get(Configuration::OPTION_REGION));
     }
 
     public function testEndpoint(): void
     {
-        $connection = Connection::from(self::ENDPOINT, self::KEY, self::SECRET, self::REGION);
-
-        $configuration = $connection->configuration();
+        $configuration = $this->connection->configuration();
 
         self::assertSame(self::ENDPOINT, $configuration->get(Configuration::OPTION_ENDPOINT));
     }
 
     public function testCredentials(): void
     {
-        $connection = Connection::from(self::ENDPOINT, self::KEY, self::SECRET, self::REGION);
-
-        $configuration = $connection->configuration();
+        $configuration = $this->connection->configuration();
 
         self::assertSame(self::KEY, $configuration->get(Configuration::OPTION_ACCESS_KEY_ID));
         self::assertSame(self::SECRET, $configuration->get(Configuration::OPTION_SECRET_ACCESS_KEY));
