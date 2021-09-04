915d29c78c70ce0fd88da2a8a0a7de3e0490d43e
Ilian Ranguelov <me@radarlog.net>
2021-09-04 20:26:15 +0300

    move trusted headers to config

--- public/index.php
+++ public/index.php
@@ -7,19 +7,6 @@ use Symfony\Component\HttpFoundation\Request;
 
 [$appEnv, $appDebug] = require_once __DIR__ . '/../bootstrap.php';
 
-// phpcs:disable SlevomatCodingStandard.Variables.DisallowSuperGlobalVariable
-
-$trustedProxies = $_ENV['TRUSTED_PROXIES'] ?? false;
-$trustedHosts = $_ENV['TRUSTED_HOSTS'] ?? false;
-
-if ($trustedProxies) {
-    Request::setTrustedProxies(explode(',', $trustedProxies), Request::HEADER_X_FORWARDED_HOST);
-}
-
-if ($trustedHosts) {
-    Request::setTrustedHosts([$trustedHosts]);
-}
-
 $kernel = new Kernel($appEnv, $appDebug);
 
 $request = Request::createFromGlobals();
