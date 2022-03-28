2f203d3d6c2154ef6c608c9159345f4c8df67a8b
Ilian Ranguelov <me@radarlog.net>
2022-03-28 21:37:33 +0200

    fix tests

--- tests/FunctionalTestCase.php
+++ tests/FunctionalTestCase.php
@@ -4,17 +4,12 @@ declare(strict_types=1);
 
 namespace Radarlog\Doop\Tests;
 
-use Radarlog\Doop\Infrastructure\Kernel;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
-use Symfony\Component\HttpKernel\KernelInterface;
 
 // phpcs:disable SlevomatCodingStandard.TypeHints.PropertyTypeHint.MissingNativeTypeHint
 class FunctionalTestCase extends KernelTestCase
 {
     use Fixtures;
-
-    /** @var Kernel|KernelInterface */
-    protected static $kernel;
 
     protected function setUp(): void
     {
